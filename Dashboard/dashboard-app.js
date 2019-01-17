ec4c6f4ea56585bdb02efd7de024d096a0a7b88d
Yaroslav Masiuk <yaryk.masyuk@gmail.com>
2019-01-17 18:24:01 +0200

    add resp style for dashboard

--- Dashboard/dashboard-app.js
+++ Dashboard/dashboard-app.js
@@ -12,6 +12,9 @@
                 var height = angular.element(document.querySelectorAll('#dashboard > div > .col-lg-9'))[0].offsetHeight;
                 angular.element(document.querySelectorAll('#dashboard > div > .col-lg-3 .user-info'))[0].style.height = height + 'px';
             }
+            else if(angular.element(document.querySelectorAll('#dashboard > div > .col-lg-3 .user-info'))[0] && angular.element(document).width() < 1200){
+                angular.element(document.querySelectorAll('#dashboard > div > .col-lg-3 .user-info'))[0].style.height = 'auto';
+            }
             
         }
     }])
