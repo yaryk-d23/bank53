bb41b2d0312e031bc6e1db36a5dc8255179dc8df
Yaroslav Masiuk <yaryk.masyuk@gmail.com>
2019-01-17 18:05:14 +0200

    add flex style

--- Dashboard/dashboard-app.js
+++ Dashboard/dashboard-app.js
@@ -8,7 +8,7 @@
             setHeight();
         },500);
         function setHeight(){
-            if(angular.element(document.querySelectorAll('#dashboard > div > .col-lg-9'))[0]) {
+            if(angular.element(document.querySelectorAll('#dashboard > div > .col-lg-9'))[0] && angular.element(document).width() >= 1200) {
                 var height = angular.element(document.querySelectorAll('#dashboard > div > .col-lg-9'))[0].offsetHeight;
                 angular.element(document.querySelectorAll('#dashboard > div > .col-lg-3 .user-info'))[0].style.height = height + 'px';
             }
