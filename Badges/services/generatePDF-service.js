8ffc47d2a32e4990295f3dc542d50a34a6ca6c55
Yaroslav Masiuk <yaryk.masyuk@gmail.com>
2019-02-20 16:59:15 +0200

    add map

---
+++ Badges/services/generatePDF-service.js
@@ -0,0 +1,153 @@
+angular.module('BadgesApp')
+    .factory('$GeneratePDF', function ($http, $q) {
+        return {
+            generatePDF: generatePDF
+        };
+
+        function getBase64Image(img) {
+            // Create an empty canvas element
+            var canvas = document.createElement("canvas");
+            canvas.width = img.width;
+            canvas.height = img.height;
+        
+            // Copy the image contents to the canvas
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(img, 0, 0);
+        
+            // Get the data-URL formatted image
+            // Firefox supports PNG and JPEG. You could check img.src to
+            // guess the original format, but be aware the using "image/jpg"
+            // will re-encode the image.
+            var dataURL = canvas.toDataURL("image/png");
+        
+            //return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+            return dataURL;
+        }
+        
+        function generatePDF($event, task) {
+            // var margins = {
+            //     top: 0,
+            //     bottom: 0,
+            //     left: 0,
+            //     width: 190
+            // };
+            var logo = angular.element($event.currentTarget.parentElement.parentElement).find('img')[0];
+            var doc = new jsPDF('p', 'pt', 'a4');
+            doc.setFontStyle('normal');
+    
+            //var logoData = getBase64Image(logo);
+            var img = new Image();
+            img.src = getBase64Image(logo);
+    
+            doc.addImage(img, 'PNG', 250, 20);
+            //doc.text(10, 40, task.Description || '');
+            var drawHeaderRow= function (row, data) {
+                //data.cursor.y += 20;
+                doc.setTextColor(68, 68, 68);
+                doc.setFillColor(255, 255, 255);
+            }
+            var table = [];
+            table.push({title:task.Description, dataKey: "title"});
+            var customHeader = {
+                cellPadding: 0,
+                //rowHeight: 30,
+                lineColor: [255, 255, 255],
+                halign: 'left',
+                valign: 'middle',
+                overflow: 'linebreak',
+                fontSize: 12,
+                fontStyle: 'normal',
+                fillColor:[255, 255, 255],
+                textColor: [0, 0, 0]
+             }
+            var style = getStyle(60,true,drawHeaderRow, undefined, undefined, customHeader);
+            doc.autoTable(table ,[] ,style);
+            doc.textWithLink(task.Title, 300, 300, {url: location.origin + location.pathname + '?task='+task.Id});
+            var data = doc.output();   
+            var buffer = new ArrayBuffer(data.length);
+            var array = new Uint8Array(buffer);
+    
+            for (var i = 0; i < data.length; i++) {
+                array[i] = data.charCodeAt(i);
+            }
+            var blob = new Blob(
+                [array],
+                {type: 'application/pdf', encoding: 'raw'}
+            );
+            
+            var reqData = {
+                fileName: task.Title.split(' ').join('_')+'_'+task.Id+'_'+task.Badge.Id+'_'+_spPageContextInfo.userId+'.pdf', 
+                arrayBuffer: blob
+            };
+            $BadgesService.uploadFile(reqData).then(function(res){
+                
+                var win = window.open(location.origin + res.ServerRelativeUrl, '_blank');
+                win.focus();
+            });
+             
+        }
+
+        function getStyle(startY,haveHeader,drawHeaderRow, drawCell, drawRow, customHeader){
+            var result ={
+                margin: {
+                            top: 40,
+                            left: 40,
+                            right: 40,
+                            bottom: 40
+                        },
+                bodyStyles: {
+                            textColor: [103, 103, 103],
+                            lineColor:[199,217,229],
+                            cellPadding: 10,
+                            //rowHeight: 30,
+                         },
+                alternateRowStyles: { 
+                            fillColor: [238, 240, 246],
+                            lineWidth: 1,
+                            lineColor:[199,217,229],
+                          },
+                styles: {
+                            fontSize: 10,
+                            valign: 'top',
+                            lineWidth: 1,
+                            lineColor:[199,217,229],
+                            overflow: 'linebreak'
+                         },
+                pageBreak: 'avoid',
+                startY:startY + 30,
+                columnStyles: {
+                    Title: {columnWidth: 150},
+                    Time: {columnWidth: 100},
+                    Solicitation: {columnWidth: 100},
+                    Status: {columnWidth: 120}
+                }
+                
+            }
+            if(drawHeaderRow)
+                result.drawHeaderRow=drawHeaderRow; 
+            if(drawCell)
+                result.drawCell = drawCell;
+            if(drawRow)
+                result.drawRow = drawRow;
+            if(haveHeader && !customHeader)
+                result.headerStyles= {
+                        cellPadding: 8,
+                        //rowHeight: 30,
+                        lineColor: [199,217,229],
+                        halign: 'left',
+                        valign: 'middle',
+                        overflow: 'visible',
+                        fontSize: 12,
+                        fontStyle: 'normal',
+                        fillColor:[222, 242, 255],
+                        textColor: [68,68,68]
+                     }
+             else if(customHeader)
+                 result.headerStyles = customHeader;
+             //else
+            //    result.headerStyles={rowHeight:0};
+            
+            return result;
+        
+        };
+    });
--- Welcome/components/welcome/welcome.js
+++ Welcome/components/welcome/welcome.js
@@ -11,8 +11,27 @@ angular.module('WelcomeApp')
 function welcomeCtrl($WelcomeService, $sce, $q){
     var ctrl = this;
     ctrl.userInfo = {};
-    $WelcomeService.getUserProfile().then(function(data){
-        angular.forEach(data.UserProfileProperties, function(prop, key){
+    ctrl.allTasks = [];
+    ctrl.allTasksLog = [];
+    ctrl.groupedTasks = [];
+    ctrl.colorArr = ['#5cb85c', '#07b16a', '#33cddd', '#245698', '#79569c'];
+    $WelcomeService.getTaskItems().then(function(res) {
+        ctrl.allTasks = res;
+        ctrl.groupedTasks = createChunksFromArra(ctrl.allTasks, 5);
+        console.log(ctrl.groupedTasks);
+    });
+
+    var requests = {
+        allTasks: $WelcomeService.getTaskItems(),
+        allTasksLog: $WelcomeService.getTaskLogItems('$filter=AssignedToId eq '+_spPageContextInfo.userId),
+        userProfile: $WelcomeService.getUserProfile()
+    };
+
+    $q.all(requests).then(function(data){
+        ctrl.allTasks = data.allTasks;
+        ctrl.groupedTasks = createChunksFromArra(ctrl.allTasks, 5);
+        ctrl.allTasksLog = data.allTasksLog;
+        angular.forEach(data.userProfile.UserProfileProperties, function(prop, key){
             if(prop.Key == "PictureURL"){
                 ctrl.userInfo.pictureUrl = prop.Value;
             }
@@ -26,10 +45,10 @@ function welcomeCtrl($WelcomeService, $sce, $q){
                 ctrl.userInfo.position = prop.Value || '';
             }
         });
-        ctrl.userInfo.fullName = data.DisplayName;
-        $WelcomeService.getUserLogItem(ctrl.userInfo.userName).then(function(data){
-            if(data.length) {
-                var user = data[0];
+        ctrl.userInfo.fullName = data.userProfile.DisplayName;
+        $WelcomeService.getUserLogItem(ctrl.userInfo.userName).then(function(res){
+            if(res.length) {
+                var user = res[0];
                 ctrl.userInfo.credits = user.Credits || 0;
                 ctrl.userInfo.xp = user.XP || 0;
                 ctrl.userInfo.userItemId = user.Id;
@@ -51,4 +70,30 @@ function welcomeCtrl($WelcomeService, $sce, $q){
             }
         });
     });
+
+    ctrl.checkTask = function(taskName, badgeId){
+        var flag = false;
+        angular.forEach(ctrl.allTasksLog,function(task, key){
+            if(task.Title == taskName && task.BadgeId == badgeId){
+                flag = true;
+            }
+        });
+        return flag;
+    }
+
+    function createChunksFromArra(array, chankLength){
+        var temparray = [];
+        var i,j;
+        for (i=0,j=array.length; i<j; i+=chankLength) {
+            temparray.push(array.slice(i,i+chankLength));
+        }
+        return temparray;
+    }
+
+    function groupBy(xs, prop) {
+        return xs.reduce(function(rv, x) {
+          (rv[x[prop]] = rv[x[prop]] || []).push(x);
+          return rv;
+        }, {});
+    }
 }
