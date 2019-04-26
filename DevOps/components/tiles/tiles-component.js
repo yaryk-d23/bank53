d2b3c4340ea7a1d955d42bfafaec22e612827fa5
Yaroslav Masiuk <yaryk.masyuk@gmail.com>
2019-04-26 04:33:57 +0300

    fix tiles

--- DevOps/components/tiles/tiles-component.js
+++ DevOps/components/tiles/tiles-component.js
@@ -13,34 +13,34 @@
 		try{
         var ctrl = this;
         ctrl.tiles = [
-            {
-                Title: 'Title1',
-                Description: 'Descriptio1',
-                Image: {Url: 'https://static.gamespot.com/uploads/original/1179/11799911/2831652-windows.jpg',},
-                Link: {Url: '#'}
-            },{
-                Title: 'New Track',
-                Description: 'Descriptio2',
-                Image: {Url: 'https://static.gamespot.com/uploads/original/1179/11799911/2831652-windows.jpg',},
-                Link: {Url: '#'}
-            },{
-                Title: 'Title3',
-                Description: 'Descriptio3',
-                Image: {Url: 'https://static.gamespot.com/uploads/original/1179/11799911/2831652-windows.jpg',},
-                Link: {Url: '#'}
-            },{
-                Title: 'Title4',
-                Description: 'Descriptio4',
-                Image: {Url: 'https://static.gamespot.com/uploads/original/1179/11799911/2831652-windows.jpg',},
-                Link: {Url: '#'}
-            },
+            // {
+            //     Title: 'Title1',
+            //     Description: 'Descriptio1',
+            //     Image: {Url: 'https://static.gamespot.com/uploads/original/1179/11799911/2831652-windows.jpg',},
+            //     Link: {Url: '#'}
+            // },{
+            //     Title: 'New Track',
+            //     Description: 'Descriptio2',
+            //     Image: {Url: 'https://static.gamespot.com/uploads/original/1179/11799911/2831652-windows.jpg',},
+            //     Link: {Url: '#'}
+            // },{
+            //     Title: 'Title3',
+            //     Description: 'Descriptio3',
+            //     Image: {Url: 'https://static.gamespot.com/uploads/original/1179/11799911/2831652-windows.jpg',},
+            //     Link: {Url: '#'}
+            // },{
+            //     Title: 'Title4',
+            //     Description: 'Descriptio4',
+            //     Image: {Url: 'https://static.gamespot.com/uploads/original/1179/11799911/2831652-windows.jpg',},
+            //     Link: {Url: '#'}
+            // },
         ];
         // $ApiService.getListItems('TSProjectTracker').then(function(res){
         //     	console.log(res);
         //     });
-		// $ApiService.getListItems('Tiles', '$filter=IsVisible eq 1').then(function(res){
-		// 	ctrl.tiles = res;
-		// });
+		$ApiService.getListItems('Tiles', '$filter=IsVisible eq 1').then(function(res){
+			ctrl.tiles = res;
+		});
         // ctrl.openNewTrackForm = function(tile){
 		// 	if(tile.Title == 'New Track'){
 		// 		$('#new-track-form').modal('show');
